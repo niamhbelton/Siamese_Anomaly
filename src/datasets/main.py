f4514e0c1693111b4c284cf2d562a142b4f7734d
Niamh Belton <niamh.belton@ucdconnect.ie>
2021-12-27 11:48:04 +0000

    adding the cifar data

--- src/datasets/main.py
+++ src/datasets/main.py
@@ -25,14 +25,12 @@ def load_dataset(dataset_name, indexes, normal_class, task, data_path, download_
 
 
     if dataset_name == 'cifar10':
-        dataset = CIFAR10_Dataset(root=data_path,
-                                  normal_class=normal_class,
-                                  indexes = indexes,
-                                  known_outlier_class=known_outlier_class,
-                                  n_known_outlier_classes=n_known_outlier_classes,
-                                  ratio_known_normal=ratio_known_normal,
-                                  ratio_known_outlier=ratio_known_outlier,
-                                  ratio_pollution=ratio_pollution)
+        dataset = CIFAR10_Dataset(indexes = indexes,
+                                root=data_path,
+                                normal_class=normal_class,
+                                task = task,
+                                data_path = data_path,
+                                download_data = download_data)
 
 
     return dataset
