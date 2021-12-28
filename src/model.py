8fe2c156d81e8c1b2de062ed77b35fde342b281d
Niamh Belton <niamh.belton@ucdconnect.ie>
2021-12-28 19:37:36 +0000

    adding lenet5'

--- src/model.py
+++ src/model.py
@@ -81,6 +81,37 @@ class MNIST_LeNet(nn.Module):
         return x
 
 
+class LeNet5(nn.Module):
+
+    def __init__(self):
+        super(LeNet5, self).__init__()
+
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1),
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1),
+            nn.Tanh()
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=120, out_features=84),
+        #    nn.Tanh(),
+        #    nn.Linear(in_features=84, out_features=n_classes),
+        )
+
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        x = torch.flatten(x, 1)
+        logits = self.classifier(x)
+        #probs = F.softmax(logits, dim=1)
+        return logits
+
+
 class cifar_lenet(nn.Module):
     def __init__(self):
         super().__init__()
